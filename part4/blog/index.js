33e1b17e2701ae44d9c7430551eeff952b5c2931
marsh5 <brodiemarshall567@gmail.com>
2021-01-13 23:03:30 -0500

    refactored into separate modules

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -1,47 +1,14 @@
 const http = require('http')
 const express = require('express')
-const app = express()
+const app = require('./app');
 const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs');
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+const server = http.createServer(app);
 
-const Blog = mongoose.model('Blog', blogSchema)
+server.listen(config.PORT, () => {
+    console.log(`Server running on port ${config.PORT}`);
+  })
 
-const mongoUrl = config.MONGODB_URI;
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/', (req,res) => {
-    res.send('<h1>Heyoyo World!!!</h1>')
-})
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-app.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`)
-})
